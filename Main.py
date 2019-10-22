cbe177bee367a79769dbbf8e5b7bc761c07f57d8
zachm7115 <zachm7115@gmail.com>
2019-10-22 15:02:45 -0400

    Webcam

--- Main.py
+++ Main.py
@@ -1,20 +1,22 @@
-import numpy as np
+
 import cv2
 
-cap = cv2.VideoCapture(0)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+def show_webcam(mirror=False):
+    cam = cv2.VideoCapture(0)
+    while True:
+        ret_val, img = cam.read()
+        if mirror:
+            img = cv2.flip(img, 1)
+        cv2.imshow('my webcam', img)
+        if cv2.waitKey(1) == 27:
+            break  # esc to quit
+    cv2.destroyAllWindows()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+def main():
+    show_webcam(mirror=True)
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
