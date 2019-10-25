126058bd658c1a27996b0f1f44a0083b1fe990ac
jaspershoe <jasper_hsu@student.wayland.k12.ma.us>
2019-10-24 21:30:16 -0400

    Circle Detection?

--- Main.py
+++ Main.py
@@ -1,34 +1,41 @@
-
-import cv2
+import cv2 as cv, numpy as np
 
 
 def show_webcam(mirror=False):
-    cam = cv2.VideoCapture(0)
+    cam = cv.VideoCapture(0)
     while True:
         ret_val, img = cam.read()
+
         if mirror:
-            img = cv2.flip(img, 1)
+            img = cv.flip(img, 1)
 
-        cv2.rectangle(img, (50, 50), (1230, 670), (0, 0, 255), 5)
+        output = img.copy()
 
-        rows = len(img)
-        cols = len(img[0])
+        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        img = cv.medianBlur(img, 5)
+        img = cv.Canny(img, 75, 150)
 
-        for r in range(rows):
-            for c in range(cols):
-                if(img[r][c])
+        circles = cv.HoughCircles(img, cv.HOUGH_GRADIENT, 1, 200, param1=100, param2=20, minRadius=1, maxRadius=100)
+        circles = np.uint16(np.around(circles))
+        for(x, y, r) in circles[0, :]:
+            cv.circle(output, (x, y), r, (0, 255, 0), 3)
+            cv.circle(output, (x, y), 2, (0, 255, 0), 3)
 
-        cv2.imshow('my webcam', img)
-        if cv2.waitKey(1) == 27:
-            break  # esc to quit
-    cv2.destroyAllWindows()
+        cv.rectangle(output, (100, 100), (1180, 620), (0,0,255), 2)
 
+        cv.imshow('Whiteboard Game', output)
+
+        #cv.imshow('Whiteboard Game', img)
+
+
+        if cv.waitKey(1) == 27:
+                break
+
+    cv.destroyAllWindows()
 
 def main():
-    show_webcam(mirror=True)
+    show_webcam(True)
 
 
 if __name__ == '__main__':
     main()
-
-    #comment
