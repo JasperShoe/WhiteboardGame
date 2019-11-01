78aa803231504c757d4858b9832422831fbb636f
jaspershoe <jasper_hsu@student.wayland.k12.ma.us>
2019-11-01 15:08:43 -0400

    STuff

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import cv2 as cv, numpy as np
 
 def show_webcam(mirror=False):
     cam = cv.VideoCapture(0)
+    background = None
 
     while True:
         ret_val, img = cam.read()
@@ -10,32 +11,40 @@ def show_webcam(mirror=False):
         if mirror:
             img = cv.flip(img, 1)
 
-        #output = img.copy()
-        # img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        # img = cv.medianBlur(img, 3)
-        # img = cv.Canny(img, 100, 150)
+        #Filters
+        img_analyze = img.copy()
+        img_analyze = cv.medianBlur(img_analyze, 3)
+        img_analyze = cv.cvtColor(img_analyze, cv.COLOR_BGR2GRAY)
 
-        img = cv.medianBlur(img, 3)
-        img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        ret, thresh = cv.threshold(img_gray, 127, 255, cv.THRESH_BINARY)
-        img_inv = cv.bitwise_not(img_gray)
+        if background is not None:
+            img_analyze = cv.subtract(background, img_analyze)
 
 
+        # #Detection Box
+        win_w = len(img_analyze[0])
+        win_h = len(img_analyze)
+        pad_x = 200
+        pad_y = 100
+        cv.rectangle(img_analyze, (0, 0), (pad_x, win_h), (0, 0, 0), thickness=-1)
+        cv.rectangle(img_analyze, (pad_x, 0), (win_w-pad_x, pad_y), (0, 0, 0), thickness=-1)
+        cv.rectangle(img_analyze, (win_w-pad_x, 0), (win_w, win_h), (0, 0, 0), thickness=-1)
+        cv.rectangle(img_analyze, (pad_x, win_h-pad_y), (win_w-pad_x, win_h), (0, 0, 0), thickness=-1)
+
+        #Contour Detection
+        ret, thresh = cv.threshold(img_analyze, 127, 255, cv.THRESH_BINARY)
         contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        cv.drawContours(img_inv, contours, -1, (0,255,0), 3)
 
-        cv.imshow("Whiteboard Game", img_inv)
+        #Drawing Contours
+        cv.drawContours(img, contours, -1, (0,255,0), 3)
 
-        # lines = cv.HoughLinesP(img, 1, np.pi/180, threshold = 100, minLineLength = 10, maxLineGap = 50)
-        # if lines is not None:
-        #     for line in lines:
-        #         x1, y1, x2, y2 = line[0]
-        #         cv.line(output, (x1, y1), (x2, y2), (255, 0, 0), 3)
-        #cv.rectangle(output, (100, 100), (1180, 620), (0, 0, 255), 2)
+        cv.imshow("Whiteboard Game", img)
 
-       # cv.imshow('Whiteboard Game', img)
-        if cv.waitKey(1) == 27:
+        k = cv.waitKey(1)
+
+        if k == 27:
             break
+        elif k == 32:
+            background = img_analyze.copy()
 
     cv.destroyAllWindows()
 
